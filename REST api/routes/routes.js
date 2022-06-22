3a6f68be3f444a636380200a33aa7f2aa82aa061
Thaifitus <nththai20@clc.fitus.edu.vn>
2022-06-22 10:29:23 +0700

    With fake JASON data

--- REST api/routes/routes.js
+++ REST api/routes/routes.js
@@ -1,26 +1,24 @@
+const users = [
+    {
+      id: 1,
+      name: "Richard Hendricks",
+      email: "richard@piedpiper.com",
+    },
+    {
+      id: 2,
+      name: "Bertram Gilfoyle",
+      email: "gilfoyle@piedpiper.com",
+    },
+  ];
 const router = (app) => {
     app.get("/", (request, response) => {
         response.send({
             message: "Node.js and Express REST API",
         });
     });
+    app.get("/users", (request, response) => {
+        response.send(users);
+    });
 };
 // Export the router
 export default router;
-
-// const users = [
-//     {
-//       id: 1,
-//       name: "Richard Hendricks",
-//       email: "richard@piedpiper.com",
-//     },
-//     {
-//       id: 2,
-//       name: "Bertram Gilfoyle",
-//       email: "gilfoyle@piedpiper.com",
-//     },
-//   ];
-//   app.get("/users", (request, response) => {
-//     response.send(users);
-// });
-// export default router;
