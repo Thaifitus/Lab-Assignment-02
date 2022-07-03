58736a42ce7fde88db62d09cbb9fb1b690f2943a
Thaifitus <nththai20@clc.fitus.edu.vn>
2022-07-03 15:03:24 +0700

    GET, POST, PUT, DELETE are completed

--- REST api/routes/routes.js
+++ REST api/routes/routes.js
@@ -3,6 +3,8 @@ import pool from '../data/config.js';
 
 // Route the app
 const router = app => {
+
+    //         GET      //
     // Display welcome message on the root
     app.get('/', (request, response) => {
         response.send({
@@ -14,6 +16,44 @@ const router = app => {
         pool.query('SELECT * FROM users', (error, result) => {
             if (error) throw error;
             response.send(result);
+        });
+    });
+    // Display a single user by ID
+    app.get('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    //          POST        //
+    // Add a new user
+    app.post('/users', (request, response) => {
+        pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+            if (error) throw error;
+            response.status(201).send(`User added with ID: ${result.insertId}`);
+        });
+    });
+
+    //      PUT     //
+    // Update an existing user
+    app.put('/users/:id', (request, response) => {
+        const id = request.params.id;
+
+        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+            if (error) throw error;
+            response.send('User updated successfully.');
+        });
+    });
+
+    //      DELETE      //
+    // Delete a user
+    app.delete('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send('User deleted.');
         });
     });
 }
